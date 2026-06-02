3ea33815434dfa0535613056d1dd71e1c41bde09
Andini Papa Sulima <andinipapasulima@gmail.com>
2026-06-02 14:21:24 +0700

    Membuat struktur dasar (Interface, Abstract Class, dan Trapesium) - Andini

--- src/projek/trapesium/Trapesium.java
+++ src/projek/trapesium/Trapesium.java
@@ -2,17 +2,12 @@ package projek.trapesium;
 
 public class Trapesium extends BangunGeometri {
 
+    public double luas;
+    public double keliling;
     public double alasAtas;
     public double alasBawah;
     public double tinggi;
-
-    public Trapesium(String nama) {
-        super(nama);
-        this.alasAtas  = 0;
-        this.alasBawah = 0;
-        this.tinggi    = 0;
-    }
-
+    
     public Trapesium(String nama, double alasAtas, double alasBawah, double tinggi) {
         super(nama);
         this.alasAtas  = alasAtas;
@@ -22,11 +17,9 @@ public class Trapesium extends BangunGeometri {
 
     @Override
     public void hitung() {
+         this.luas     = 0.5 * (alasAtas + alasBawah) * tinggi;
         double selisihAlas = Math.abs(alasBawah - alasAtas) / 2.0;
-        double miring      = Math.sqrt((selisihAlas * selisihAlas) + (tinggi * tinggi));
-
-        this.luas     = 0.5 * (alasAtas + alasBawah) * tinggi;
-        this.keliling = alasAtas + alasBawah + (2.0 * miring);
-        this.volume   = 0.0;
+        double sisiMiring      = Math.sqrt((selisihAlas * selisihAlas) + (tinggi * tinggi));
+        this.keliling = alasAtas + alasBawah + (2.0 * sisiMiring);
     }
 }
